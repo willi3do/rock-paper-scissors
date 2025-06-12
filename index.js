f5ad3f0db3fa24f05cb7d8a20f2ae5d629733d55
willi3do <dominguez.william@yahoo.com>
2025-06-11 22:38:35 -0700

    add: computer choice with an object that returns a random number.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+/* function get computer choice will
+return rock, paper, scissors at random. */
+
+function getComputerChoice() {
+   const choice ={
+    1: "rock",
+    2: "paper",
+    3: "scissors"
+    };
+
+    const randomNum = Math.floor(Math.random() * 3) + 1;
+
+    return choice[randomNum];
+}
+
+console.log(getComputerChoice());
+
+
+// const userInput = document.getElementById("userInput");
+
+// function getHumanChoice() {
+// userInput.
+// } {
+    
+// }
+/* get humans choice and return a valid choice for the user
+input. Us prompt method to get user's input. no error handling 
+for now. */
