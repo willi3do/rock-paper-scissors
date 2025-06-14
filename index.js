207d2e12eb50ac23b89f6e903bc9849c7a286e58
willi3do <dominguez.william@yahoo.com>
2025-06-13 17:28:03 -0700

    add: 3 functions. 1. add: Computer choice to call them later.  2. player choice for user input.  3.add: logice for the game in playRounds.

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
 /* function get computer choice will
 return rock, paper, scissors at random. */
 
+let humanScore = 0;
+let computerScore = 0;
+
+function getChoices() {
+   return ["rock", "paper", "scissors"];
+};
+
 function getComputerChoice() {
-   const choice ={
-    1: "rock",
-    2: "paper",
-    3: "scissors"
-    };
+    const choices = getChoices();
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 
-    const randomNum = Math.floor(Math.random() * 3) + 1;
-
-    return choice[randomNum];
 }
 
-console.log(getComputerChoice());
 
 
-// const userInput = document.getElementById("userInput");
 
-// function getHumanChoice() {
-// userInput.
-// } {
-    
-// }
 /* get humans choice and return a valid choice for the user
 input. Us prompt method to get user's input. no error handling 
 for now. */
+
+function getPlayerChoice(){
+    const userInput = prompt("Enter rock, paper or scissors.");
+    return userInput.trim().toLocaleLowerCase();
+}
+
+
+function playRound() {
+    const player = getPlayerChoice();
+    const computer = getComputerChoice();
+
+    console.log("Player:", player);
+    console.log("Computer:", computer);
+            
+  if (player === computer){ // input if you tie.
+    console.log(player + " It's a tie! Try again.");
+
+    // input if you Lose.
+    } else if (player === "rock" && computer === "paper") {
+        console.log("paper beats rock! Try Again.");
+    }else if(player === "paper" && computer === "scissors"){
+        console.log("scissors beat paper! Try Again.");
+    }else if(player === "scissors" && computer === "rock"){
+        console.log("rock beats scissors! try again");
+    // input if you WIN
+    } else if (player === "rock" && computer === "scissors"){
+        console.log("rock beats scissors. You Win!");
+    }else if(player === "paper" && computer === "rock"){
+        console.log("paper beat rock. You Win!");
+    }else if(player === "scissors" && computer === "paper"){
+        console.log("scissors beats paper. You Win!");
+    }else{
+        prompt("Invalid input. Please enter rock, paper, or scissors.")
+        playRound(console.log());
+    }};
+playRound([5]);
+
+    // function playRound(getComputerChoice, userInput) {
+    // if (getComputerChoice === userInput) {
+    //     return console.log(" its a tie");
+    // }
